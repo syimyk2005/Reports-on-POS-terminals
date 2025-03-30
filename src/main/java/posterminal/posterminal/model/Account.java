422c66fe619b5dc34a5bd96574c15aadaaf3459e
syimyk <sagynov512@gmail.com>
2025-03-30 23:27:32 +0600

    added model Account

---
+++ src/main/java/posterminal/posterminal/model/Account.java
@@ -0,0 +1,21 @@
+package posterminal.posterminal.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double balance;
+}
+
