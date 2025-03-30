89c86d948ae1700bbf3526fe06ed03ffcc9d422c
syimyk <sagynov512@gmail.com>
2025-03-30 23:33:30 +0600

    added service TransactionService

--- src/main/java/posterminal/posterminal/model/Account.java
+++ src/main/java/posterminal/posterminal/model/Account.java
@@ -6,7 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
---
+++ src/main/java/posterminal/posterminal/service/TransactionService.java
@@ -0,0 +1,37 @@
+package posterminal.posterminal.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import posterminal.posterminal.model.Account;
+import posterminal.posterminal.repository.AccountRepository;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Transactional
+    public String transferMoney(String fromAccountName, String toAccountName, double amount) {
+        Account fromAccount = accountRepository.findByName(fromAccountName);
+        Account toAccount = accountRepository.findByName(toAccountName);
+
+        if (fromAccount == null || toAccount == null) {
+            throw new IllegalArgumentException("Один из счетов не найден");
+        }
+
+        if (fromAccount.getBalance() < amount) {
+            throw new IllegalArgumentException("Недостаточно средств для перевода");
+        }
+
+        fromAccount.withdraw(amount);
+        toAccount.deposit(amount);
+
+        accountRepository.save(fromAccount);
+        accountRepository.save(toAccount);
+
+        return "Транзакция успешно выполнена";
+    }
+}
+
